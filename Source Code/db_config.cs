45ca73afe316198db330d71f5d24403603c0d522
Daryl Gatt <42916447+phantom0004@users.noreply.github.com>
2024-03-28 11:10:17 +0100

    Commit new code

---
+++ Source Code/db_config.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace MyConsoleApp
+{
+    public class DatabaseConnector
+    {
+        // Function to create and return a database connection
+        public static MySqlConnection GetDatabaseConnection()
+        {
+            // Read credentials from the file
+            string[] credentials = File.ReadAllLines("C:\\Users\\sapia\\Desktop\\Panarama Market\\pass.txt");
+            string username = credentials.Length > 0 ? credentials[0] : "";
+            string password = credentials.Length > 1 ? credentials[1] : "";
+
+            // Construct the connection string
+            string connectionString = $"server=localhost;user={username};password={password};database=ecommerce";
+
+            // Create a new MySqlConnection object
+            var connection = new MySqlConnection(connectionString);
+
+            return connection;
+        }
+    }
+}
---
+++ Source Code/main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Panarama_Market
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+    }
+}
